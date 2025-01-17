e93235ddff6383d1af46e1a52ec48a63727033ba
Piyush7034 <piyushshukla2100@gmail.com>
2025-01-17 20:09:30 +0530

    Added security config changes
    Signed-off-by: Piyush7034 <piyushshukla2100@gmail.com>

--- certify-service/src/main/java/io/mosip/certify/config/SecurityConfig.java
+++ certify-service/src/main/java/io/mosip/certify/config/SecurityConfig.java
@@ -19,6 +19,10 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.csrf.CsrfTokenRepository;
+import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
 
 import java.util.List;
@@ -56,13 +60,35 @@ public class SecurityConfig {
     private String[] ignoreCsrfCheckUrls;
 
     @Bean
-    public SecurityFilterChain web(HttpSecurity http) throws Exception {
+    public SecurityFilterChain web(HttpSecurity http, HandlerMappingIntrospector introspector) throws Exception {
+        MvcRequestMatcher.Builder mvcMatcherBuilder = new MvcRequestMatcher.Builder(introspector)
+                .servletPath(servletPath);
 
-        http.csrf(httpEntry -> httpEntry.ignoringRequestMatchers(ignoreCsrfCheckUrls)
+        RequestMatcher[] csrfIgnoreMatchers = new RequestMatcher[ignoreCsrfCheckUrls.length];
+
+        for (int i = 0; i < ignoreCsrfCheckUrls.length; i++) {
+            if (ignoreCsrfCheckUrls[i].startsWith("/services/")) {
+                csrfIgnoreMatchers[i] = new AntPathRequestMatcher(ignoreCsrfCheckUrls[i]);
+            } else {
+                csrfIgnoreMatchers[i] = mvcMatcherBuilder.pattern(ignoreCsrfCheckUrls[i]);
+            }
+        }
+
+        RequestMatcher[] authIgnoreMatchers = new RequestMatcher[ignoreAuthUrls.length];
+
+        for (int i = 0; i < ignoreAuthUrls.length; i++) {
+            if (ignoreAuthUrls[i].startsWith("/services/")) {
+                authIgnoreMatchers[i] = new AntPathRequestMatcher(ignoreAuthUrls[i]);
+            } else {
+                authIgnoreMatchers[i] = mvcMatcherBuilder.pattern(ignoreAuthUrls[i]);
+            }
+        }
+
+        http.csrf(httpEntry -> httpEntry.ignoringRequestMatchers(csrfIgnoreMatchers)
                 .csrfTokenRepository(this.getCsrfTokenRepository()));
 
         http.authorizeHttpRequests(authorizeRequests -> authorizeRequests
-                .requestMatchers(ignoreAuthUrls).permitAll()
+                .requestMatchers(authIgnoreMatchers).permitAll()
                 .anyRequest().authenticated()
         ).oauth2ResourceServer(oauth2 -> oauth2
                 .jwt(jwt -> jwt
