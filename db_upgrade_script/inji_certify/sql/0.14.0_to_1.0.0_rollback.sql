024d476cc7e7c8f4736d908da72717ab31f77612
Sakshiijk <sakshi.jhanwar@ad.infosys.com>
2026-06-09 14:34:55 +0530

    Update pom version and DB upgrade scripts to 1.0.0
    Signed-off-by: Sakshiijk <sakshi.jhanwar@ad.infosys.com>

---
+++ db_upgrade_script/inji_certify/sql/0.14.0_to_1.0.0_rollback.sql
@@ -0,0 +1,5 @@
+-- Rollback script from 1.0.0 to 0.14.0
+-- Version 1.0.0 Release
+
+-- No schema changes to rollback for this release
+
---
+++ db_upgrade_script/inji_certify/sql/0.14.0_to_1.0.0_upgrade.sql
@@ -0,0 +1,5 @@
+-- Upgrade script from 0.14.0 to 1.0.0
+-- Version 1.0.0 Release
+
+-- No schema changes required for this release
+
