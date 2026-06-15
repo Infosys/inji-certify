e0e91cd0fc0db9f5f7d9a9c3905a33b1afe91c40
Sakshiijk <sakshi.jhanwar@ad.infosys.com>
2026-06-15 12:10:16 +0530

    updated rollback and upgrade files
    Signed-off-by: Sakshiijk <sakshi.jhanwar@ad.infosys.com>

--- db_upgrade_script/inji_certify/sql/0.14.0_to_1.0.0_rollback.sql
+++ db_upgrade_script/inji_certify/sql/0.14.0_to_1.0.0_rollback.sql
@@ -1,5 +1,29 @@
--- Rollback script from 1.0.0 to 0.14.0
--- Version 1.0.0 Release
+UPDATE certify.credential_config
+SET display = COALESCE((
+    SELECT jsonb_agg(
+                   CASE
+                       WHEN elem->'logo' IS NOT NULL
+                           AND (elem->'logo')::jsonb ? 'uri' THEN
+                           jsonb_set(
+                                   elem::jsonb,
+                                   '{logo}',
+                                   ((elem->'logo')::jsonb - 'uri')
+                    || jsonb_build_object(
+                        'url',
+                        (elem->'logo')::jsonb -> 'uri'
+                    )
+                )
+                       ELSE elem::jsonb
+                       END
+           )
+    FROM jsonb_array_elements(display::jsonb) AS elem
+), '[]'::jsonb)
+WHERE display IS NOT NULL;
 
--- No schema changes to rollback for this release
+ALTER TABLE certify.credential_config
+RENAME COLUMN claims TO credential_subject;
+COMMENT ON COLUMN certify.credential_config.credential_subject IS 'Credential Subject: JSON object containing subject attributes schema.';
 
+UPDATE certify.credential_config
+SET credential_format = 'vc+sd-jwt'
+WHERE credential_format = 'dc+sd-jwt';
--- db_upgrade_script/inji_certify/sql/0.14.0_to_1.0.0_upgrade.sql
+++ db_upgrade_script/inji_certify/sql/0.14.0_to_1.0.0_upgrade.sql
@@ -1,5 +1,26 @@
--- Upgrade script from 0.14.0 to 1.0.0
--- Version 1.0.0 Release
+UPDATE certify.credential_config
+SET display = COALESCE((
+    SELECT jsonb_agg(
+                   CASE
+                       WHEN elem->'logo' IS NOT NULL
+                           AND (elem->'logo')::jsonb ? 'url' THEN
+                           jsonb_set(
+                                   elem::jsonb,
+                                   '{logo}',
+                                   ((elem->'logo')::jsonb - 'url')
+                    || jsonb_build_object('uri', (elem->'logo')::jsonb -> 'url')
+                )
+                       ELSE elem::jsonb
+                       END
+           )
+    FROM jsonb_array_elements(display::jsonb) AS elem
+), '[]'::jsonb)
+WHERE display IS NOT NULL;
 
--- No schema changes required for this release
+ALTER TABLE certify.credential_config
+RENAME COLUMN credential_subject TO claims;
+COMMENT ON COLUMN certify.credential_config.claims IS 'Claims: JSON object containing subject attributes schema.';
 
+UPDATE certify.credential_config
+SET credential_format = 'dc+sd-jwt'
+WHERE credential_format = 'vc+sd-jwt';
