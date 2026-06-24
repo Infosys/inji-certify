364059c1aa0c21048faee12643cfcb3085b5a1bc
Sakshiijk <sakshi.jhanwar@ad.infosys.com>
2026-06-24 16:06:49 +0530

    [INJICERT-#902]: Update db_scripts
    Signed-off-by: Sakshiijk <sakshi.jhanwar@ad.infosys.com>

--- db_upgrade_script/inji_certify/sql/0.14.0_to_1.0.0_rollback.sql
+++ db_upgrade_script/inji_certify/sql/0.14.0_to_1.0.0_rollback.sql
@@ -27,3 +27,12 @@ COMMENT ON COLUMN certify.credential_config.credential_subject IS 'Credential Su
 UPDATE certify.credential_config
 SET credential_format = 'vc+sd-jwt'
 WHERE credential_format = 'dc+sd-jwt';
+
+DROP INDEX IF EXISTS certify.idx_vp_submission_response_code;
+DROP INDEX IF EXISTS certify.idx_vc_submission_transaction_id;
+DROP INDEX IF EXISTS certify.idx_ard_transaction_id;
+
+DROP TABLE IF EXISTS certify.vp_submission;
+DROP TABLE IF EXISTS certify.vc_submission;
+DROP TABLE IF EXISTS certify.presentation_definition;
+DROP TABLE IF EXISTS certify.authorization_request_details;
